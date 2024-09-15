a7195b8e890ea8bc6f71fb40dfd8a85066e9f3f5
Leonardo Bertonasco <leonardo.bertonasco01@gmail.com>
2024-09-15 10:05:43 +0200

    new config

---
+++ nvim/my_nvim/lua/core/plugins/colorscheme_light.lua
@@ -0,0 +1,10 @@
+-- return {
+-- 	"ellisonleao/gruvbox.nvim",
+-- 	lazy = false,
+-- 	priority = 1000,
+-- 	config = function()
+-- 		-- Active le mode clair pour gruvbox
+-- 		vim.o.background = "light"
+-- 		vim.cmd("colorscheme gruvbox")
+-- 	end,
+-- }
