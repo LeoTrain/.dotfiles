cd45257a26d5f9d68f69c31432d618a36903f6b1
Leonardo Bertonasco <leonardo.bertonasco01@gmail.com>
2024-10-30 12:57:07 +0100

    nvchad

--- nvim/.config/nvim/config/lualine.lua
+++
@@ -1,7 +0,0 @@
--- config/lualine.lua
--- Status line configuration
-
-require("lualine").setup({
-    options = { theme = "gruvbox", section_separators = "", component_separators = "" },
-})
-
--- nvim/.config/nvim/config/treesitter.lua
+++
@@ -1,7 +0,0 @@
-require("nvim-treesitter.configs").setup({
-    ensure_installed = { "lua", "python", "c_sharp" },
-    highlight = { enable = true },
-    incremental_selection = { enable = true },
-    textobjects = { enable = true },
-})
-
