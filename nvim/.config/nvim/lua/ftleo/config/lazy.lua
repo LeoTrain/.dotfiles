a9673348481ba01b245534e3e605fc58c7c3a400
Leonardo Bertonasco <leonardo.bertonasco01@gmail.com>
2024-12-02 01:11:31 +0100

    colorscheme

--- nvim/.config/nvim/lua/ftleo/config/lazy.lua
+++ nvim/.config/nvim/lua/ftleo/config/lazy.lua
@@ -15,7 +15,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup("ftleo.plugins")
 
 -- After Lazy
-vim.cmd.colorscheme("night-owl")
+vim.cmd.colorscheme("kanagawa")
 vim.api.nvim_set_hl(0, "GitSignsAdd", { fg = "#00ff00", bg = "none" })
 vim.api.nvim_set_hl(0, "GitSignsChange", { fg = "#ffff00", bg = "none" })
 vim.api.nvim_set_hl(0, "GitSignsDelete", { fg = "#ff0000", bg = "none" })
--- nvim/.config/nvim/lua/ftleo/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/ftleo/plugins/colorscheme.lua
@@ -1,11 +1,33 @@
 return {
-    "oxfist/night-owl.nvim",
-    require("lazy").setup({
-      "oxfist/night-owl.nvim",
-      lazy = false, -- make sure we load this during startup if it is your main colorscheme
-      priority = 1000, -- make sure to load this before all the other start plugins
-      config = function()
-        require("night-owl").setup()
-      end,
-    })
+    "rebelot/kanagawa.nvim",
+    lazy = false,
+    priority = 1000, -- Charge ce plugin avant les autres pour appliquer le thème immédiatement
+    config = function()
+        require("kanagawa").setup({
+            compile = false, -- Ne compile pas le schéma de couleurs pour garder la flexibilité
+            undercurl = true, -- Ajoute des soulignements stylés
+            commentStyle = { italic = true },
+            functionStyle = { bold = true },
+            keywordStyle = { italic = true },
+            statementStyle = { bold = true },
+            typeStyle = { bold = true },
+            transparent = false, -- Met à true pour une transparence de l'arrière-plan
+            dimInactive = false, -- Diminue les fenêtres inactives
+            terminalColors = true, -- Applique les couleurs au terminal intégré
+            colors = {
+                theme = {
+                    all = {
+                        ui = {
+                            bg_gutter = "none", -- Enlève la couleur du gutter si nécessaire
+                        },
+                    },
+                },
+            },
+            overrides = function(colors)
+                return {
+                    Normal = { bg = "none" }, -- Rendre l'arrière-plan transparent
+                }
+            end,
+        })
+    end,
 }
