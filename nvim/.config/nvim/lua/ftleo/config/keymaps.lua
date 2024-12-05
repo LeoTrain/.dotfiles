fd334fdf11a8a4f19ff9e1cb0fe6e56ee57a0433
Leonardo Bertonasco <leonardo.bertonasco01@gmail.com>
2024-12-06 00:04:03 +0100

    added splits and movement

--- nvim/.config/nvim/lua/ftleo/config/keymaps.lua
+++ nvim/.config/nvim/lua/ftleo/config/keymaps.lua
@@ -4,8 +4,17 @@ vim.g.mapleader = " "
 keymap('i', 'df', '<ESC>', {})
 keymap('n', '<leader>e', ':Ex<CR>', {})
 
-keymap('n', '<leader>n', ':bn<CR>', { noremap = true, desc = "Buffer next"})
-keymap('n', '<leader>p', ':bp<CR>', { noremap = true, desc = "Buffer previous"})
-keymap('n', '<leader>x', ':bd<CR>', { noremap = true, desc = "Buffer delete"})
+keymap('n', '<leader>wn', ':bn<CR>', { noremap = true, desc = "Buffer next"})
+keymap('n', '<leader>wp', ':bp<CR>', { noremap = true, desc = "Buffer previous"})
+keymap('n', '<leader>wx', ':bd<CR>', { noremap = true, desc = "Buffer delete"})
+
+keymap('n', '<leader>sp', ':sp<CR>', { noremap = true, desc = "Split horizontally"})
+keymap('n', '<leader>sv', ':vs<CR>', { noremap = true, desc = "Split vertically"})
+keymap('n', '<leader>sh', '<c-w>h', { noremap = true, desc = "Go to split left"})
+keymap('n', '<leader>sj', '<c-w>j', { noremap = true, desc = "Go to split down"})
+keymap('n', '<leader>sk', '<c-w>k', { noremap = true, desc = "Go to split up"})
+keymap('n', '<leader>sl', '<c-w>l', { noremap = true, desc = "Go to split right"})
+
 keymap('n', '<leader>q', ':q<CR>', { noremap = true, desc = "Close window"})
 keymap('n', '<leader>w', ':w<CR>', { noremap = true, desc = "Buffer save"})
+
