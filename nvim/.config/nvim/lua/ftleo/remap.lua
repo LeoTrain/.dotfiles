10e3ad7e0eff68e64ba520ee63b1083b8ac6b7f1
Leonardo Bertonasco <leonardo.bertonasco01@gmail.com>
2024-12-26 01:10:16 +0100

    colorscheme and other stuff

--- nvim/.config/nvim/lua/ftleo/remap.lua
+++ nvim/.config/nvim/lua/ftleo/remap.lua
@@ -13,5 +13,12 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("x", "<leader>p", "\"_dP")
 
 -- Switch tmux session
-vim.keymap.set("n", "<C-s>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+-- Go back to normal mode
+vim.keymap.set("i", "jj", "<Esc>")
+
+-- Tabs
+vim.keymap.set("n", "<leader>to", vim.cmd.tabnew)
+vim.keymap.set("n", "<leader>tx", vim.cmd.tabclose)
