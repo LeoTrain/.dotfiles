e95e81254fa024dd07113fe18d5ce126c8a67510
Leonardo Bertonasco <leonardo.bertonasco01@gmail.com>
2025-01-02 03:12:58 +0100

    modified to moccha

--- nvim/nvim/lua/mario/plugins/catppuccin.lua
+++ nvim/nvim/lua/mario/plugins/catppuccin.lua
@@ -1,17 +1,17 @@
 return {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-        require("catppuccin").setup({
-            flavour = "mocha",                          -- latte, frappe, macchiato, mocha
-            integrations = {
-                cmp = true,
-                gitsigns = true,
-                nvimtree = true,
-                notify = true,
-            }
-        })
-        vim.cmd.colorscheme "catppuccin"
-    end
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	config = function()
+		require("catppuccin").setup({
+			flavour = "mocha", -- latte, frappe, macchiato, mocha
+			integrations = {
+				cmp = true,
+				gitsigns = true,
+				nvimtree = true,
+				notify = true,
+			},
+		})
+		vim.cmd.colorscheme("catppuccin")
+	end,
 }
