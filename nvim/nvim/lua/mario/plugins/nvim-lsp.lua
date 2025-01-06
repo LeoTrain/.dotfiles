5f80ffddd636e9fb26e7e27df781de2c2d2ce771
Leonardo Bertonasco <leonardo.bertonasco01@gmail.com>
2025-01-06 02:49:54 +0100

    bash basic lsp

--- nvim/nvim/lua/mario/plugins/nvim-lsp.lua
+++ nvim/nvim/lua/mario/plugins/nvim-lsp.lua
@@ -10,6 +10,9 @@ return {
         lspconfig.pylsp.setup({
             capabilities = capabilities
         })
+        lspconfig.bashls.setup({
+            capabilities = capabilities
+        })
 
         vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
         vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
