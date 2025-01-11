d40fd338ed612371025986c2b8863f0ef4265279
Leonardo Bertonasco <leonardo.bertonasco01@gmail.com>
2025-01-11 06:16:13 +0100

    added bindings to lsp.buf methods

--- nvim/nvim/lua/mario/plugins/nvim-lsp.lua
+++ nvim/nvim/lua/mario/plugins/nvim-lsp.lua
@@ -16,6 +16,9 @@ return {
 
         vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
         vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references, {})
+        vim.keymap.set('n', 're', vim.lsp.buf.rename, {})
+        vim.keymap.set('n', '<leader>de', vim.lsp.buf.declaration, {})
         vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
     end,
 }
