3a64990ff6f2aa0b81c2c7fa484fea5fdf1fd7af
Leonardo Bertonasco <leonardo.bertonasco01@gmail.com>
2025-01-12 17:27:52 +0100

    status bar and bufferline

--- nvim/nvim/lua/mario/plugins/bufferline.lua
+++ nvim/nvim/lua/mario/plugins/bufferline.lua
@@ -7,8 +7,16 @@ return {
 			mode = "tabs",
 			separator_style = "slant",
 		},
+		highlights = {
+			buffer_selected = {
+				fg = { attribute = "fg", highlight = "Normal" },
+				bg = { attribute = "bg", highlight = "Normal" },
+				bold = true,
+				underline = true, -- This will add an underline under the active buffer
+			},
+		},
 	},
-    config = function(_, opts)
-        require("bufferline").setup(opts)
-    end,
+	config = function(_, opts)
+		require("bufferline").setup(opts)
+	end,
 }
