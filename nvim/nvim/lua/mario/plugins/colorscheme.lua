c8a632c3cbcf7b77da0b909253eef723857fba00
Leonardo Bertonasco <leonardo.bertonasco01@gmail.com>
2025-01-12 16:25:48 +0100

    no neck pain ui

--- nvim/nvim/lua/mario/plugins/colorscheme.lua
+++ nvim/nvim/lua/mario/plugins/colorscheme.lua
@@ -1,38 +1,46 @@
 return {
-    -- "catppuccin/nvim",
-    -- name = "catppuccin",
-    -- priority = 1000,
-    -- config = function()
-    -- 	require("catppuccin").setup({
-    -- 		flavour = "mocha", -- latte, frappe, macchiato, mocha
-    -- 		integrations = {
-    -- 			cmp = true,
-    -- 			gitsigns = true,
-    -- 			nvimtree = true,
-    -- 			notify = true,
-    -- 		},
-    -- 	})
-    -- 	vim.cmd.colorscheme("catppuccin")
-    -- end,
-
-    -- "comfysage/evergarden",
-    -- priority = 1000, -- Colorscheme plugin is loaded first before any other plugins
-    -- opts = {
-    --     transparent_background = false,
-    --     variant = "medium", -- 'hard'|'medium'|'soft'
-    --     overrides = {}, -- add custom overrides
-    -- },
-    -- config = function()
-    --     require('evergarden').setup({})
-    --     vim.cmd.colorscheme("evergarden")
-    -- end,
-
-    "tiagovla/tokyodark.nvim",
-    opts = {
-        -- custom options here
-    },
-    config = function(_, opts)
-        require("tokyodark").setup(opts) -- calling setup is optional
-        vim.cmd([[colorscheme tokyodark]])
-    end,
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				flavour = "mocha", -- latte, frappe, macchiato, mocha
+				integrations = {
+					cmp = true,
+					gitsigns = true,
+					nvimtree = true,
+					notify = true,
+				},
+			})
+		end,
+	},
+	-- {
+	-- "comfysage/evergarden",
+	-- priority = 1000, -- Colorscheme plugin is loaded first before any other plugins
+	-- opts = {
+	--     transparent_background = false,
+	--     variant = "medium", -- 'hard'|'medium'|'soft'
+	--     overrides = {}, -- add custom overrides
+	-- },
+	-- config = function()
+	--     require('evergarden').setup({})
+	--     vim.cmd.colorscheme("evergarden")
+	-- end,
+	-- }
+	{
+		"tiagovla/tokyodark.nvim",
+        priority = 10001,
+		opts = {
+			-- custom options here
+            cmp = true,
+            gitsigns = true,
+            nvimtree = true,
+            notify = true,
+		},
+		config = function(_, opts)
+			require("tokyodark").setup(opts) -- calling setup is optional
+			vim.cmd([[colorscheme tokyodark]])
+		end,
+	},
 }
--- nvim/nvim/lua/mario/plugins/no-neck-pain.lua
+++ nvim/nvim/lua/mario/plugins/no-neck-pain.lua
@@ -1,21 +1,30 @@
 return {
-	"shortcuts/no-neck-pain.nvim",
-	version = "*",
-	config = function()
-		require("no-neck-pain").setup({
+    "shortcuts/no-neck-pain.nvim",
+    version = "*",
+    config = function()
+        -- local palette = require("catppuccin.palettes").get_palette("mocha")
+        require("no-neck-pain").setup({
+            width=100,
             autocmds = {
                 enableOnVimEnter = true,
                 enableOnTabEnter = true,
             },
-			buffers = {
-				scratchPad = {
-					enabled = true,
-					location = "~/lab/notes",
-				},
-				bo = {
-					filetype = "md",
-				},
-			},
-		})
-	end,
+            buffers = {
+                scratchPad = {
+                    enabled = true,
+                    location = "~/lab/notes",
+                },
+                left = {
+                    colors = {
+                        background = "#414868",
+                        blend = -0.63
+                    },
+                },
+                bo = {
+                    filetype = "md",
+                },
+                enable_in_split = true,
+            },
+        })
+    end,
 }
