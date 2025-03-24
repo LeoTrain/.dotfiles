28f1aec70aa86f25a228bf966a8665dcc5849607
FT_Leo <ftleo@Leonardos-MacBook-Air.local>
2025-03-24 09:39:27 +0100

    small modification fzf lua

--- nvim/lua/plugins/fzf_lua.lua
+++ nvim/lua/plugins/fzf_lua.lua
@@ -4,26 +4,39 @@ return {
 	-- dependencies = { "nvim-tree/nvim-web-devicons" },
 	-- or if using mini.icons/mini.nvim
 	dependencies = { "echasnovski/mini.icons" },
-	opts = {},
+	opts = {
+		border = "single",
+		row = 0,
+		col = 0,
+	},
 	keys = {
 		{
 			"<leader>ff",
-			function() require('fzf-lua').files() end,
-			desc="[F]ind [F]iles"
+			function()
+				require("fzf-lua").files()
+			end,
+			desc = "[F]ind [F]iles",
 		},
 		{
 			"<leader>fg",
-			function() require('fzf-lua').live_grep() end,
+			function()
+				require("fzf-lua").live_grep()
+			end,
+			desc = "[F]ind [G]rep"
 		},
 		{
 			"<leader>fb",
-			function() require('fzf-lua').buffers() end,
-			desc="[F]ind [B]uffers"
+			function()
+				require("fzf-lua").buffers()
+			end,
+			desc = "[F]ind [B]uffers",
 		},
 		{
 			"<leader>f.",
-			function() require('fzf-lua').builtin() end,
-			desc="[F]ind [.]Idon'tknow"
+			function()
+				require("fzf-lua").builtin()
+			end,
+			desc = "[F]ind [.]Idon'tknow",
 		},
-	}
+	},
 }
